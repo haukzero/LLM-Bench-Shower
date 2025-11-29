e4092cc6dbae5a9aafe5a33176c7eea7ab111488
haukzero <haukzero1@gmail.com>
2025-11-29 16:09:59 +0800

    feat(config): introduce local configuration override for dataset paths
    - Separate the dataset path configuration files `default_dataset_paths.json` and `local_dataset_paths.json`. Developers should fill in the latter according to their actual environment; the latter's content will override the former.
    - `local_dataset_paths.json` has been added to `.gitignore` to ensure it doesn't pollute public repo.

--- LLMBenchShower/backend/bench/utils.py
+++ LLMBenchShower/backend/bench/utils.py
@@ -4,7 +4,9 @@ from typing import List, Dict
 
 CONFIG_DIR = os.path.join(os.path.dirname(__file__), "../../configs/")
 SUB_DATASETS_PATH = os.path.join(CONFIG_DIR, "sub_datasets.json")
-DATASET_PATHS_PATH = os.path.join(CONFIG_DIR, "dataset_paths.json")
+DEFAULT_DATASET_PATHS_PATH = os.path.join(CONFIG_DIR, "default_dataset_paths.json")
+LOCAL_DATASET_PATHS_PATH = os.path.join(CONFIG_DIR, "local_dataset_paths.json")
+DATASET_PATHS: Dict[str, str] | None = None
 
 
 def get_sub_datasets(dataset_name: str) -> List[str]:
@@ -18,8 +20,20 @@ def get_available_datasets() -> Dict[str, List[str]]:
         supported_datasets = json.load(f)
     return supported_datasets
 
+def _load_dataset_paths():
+    global DATASET_PATHS
+    with open(DEFAULT_DATASET_PATHS_PATH, "r") as f:
+        dps = json.load(f)
+    if os.path.exists(LOCAL_DATASET_PATHS_PATH):
+        with open(LOCAL_DATASET_PATHS_PATH, "r") as f:
+            local_paths = json.load(f)
+        dps |= local_paths
+    DATASET_PATHS = dps
 
 def get_dataset_path(dataset_name: str) -> str:
-    with open(DATASET_PATHS_PATH, "r") as f:
-        dataset_paths = json.load(f)
-    return dataset_paths.get(dataset_name, "")
+    global DATASET_PATHS
+    if DATASET_PATHS is None:
+        _load_dataset_paths()
+    if dataset_name not in DATASET_PATHS:
+        raise ValueError(f"Dataset {dataset_name} not found in dataset paths.")
+    return DATASET_PATHS[dataset_name]
